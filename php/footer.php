43a7064d16efe4b0f476877f086797d8c8105512
Prd46 <prd46@drexel.edu>
2022-10-21 10:15:46 -0400

    Font changes

--- php/footer.php
+++ php/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="footer__fullBox">
         <div class="footer__contentBox">
-            <h2>&#169; Paul DiGerolamo, 2022</h2>
+            <h2>&#169; <?php echo date('Y'); ?> Paul DiGerolamo</h2>
             <div class="footer__contacts">
                 <a href="mailto:Prd46@drexel.edu"><img src="media/mailIcon.png" alt="mail" class="footer__icon" /></a>
                 <a href="tel:6098326844"><img src="media/phoneIcon.png" alt="phone" class="footer__icon" /></a>
